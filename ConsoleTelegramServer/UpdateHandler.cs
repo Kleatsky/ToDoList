0f9fdcabee192217b4957778d004fbf950b4a238
salaga999999999 <salaga999999999@gmail.com>
2025-06-03 03:28:08 +0700

    Clear code

--- ConsoleTelegramServer/UpdateHandler.cs
+++ ConsoleTelegramServer/UpdateHandler.cs
@@ -32,8 +32,6 @@ namespace ConsoleTelegramServer
     {
 
         public delegate void MessageHandler(object sender, string message);
-        //public event MessageHandler OnHandleUpdateStarted;
-        //public event MessageHandler OnHandleUpdateCompleted;
         public enum Step
         {
             WaitingForType,
@@ -167,7 +165,7 @@ namespace ConsoleTelegramServer
             }
         }
 
-        private static async Task<bool> IsUserAdding(ITelegramBotClient bot, Update update, CancellationToken cancellationToken,
+        private async Task<bool> IsUserAdding(ITelegramBotClient bot, Update update, CancellationToken cancellationToken,
                     long chatId)
         {
             if (UserAdding.TryGetValue(chatId, out var stepTask))
